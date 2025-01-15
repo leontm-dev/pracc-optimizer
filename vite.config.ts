e248a1ca67962a4193d85af2383b5b8c2b7368d2
LeonTM <lanneken09@gmail.com>
2025-01-15 21:19:05 +0100

    Added both new content scripts

--- vite.config.ts
+++ vite.config.ts
@@ -17,9 +17,15 @@ export default defineConfig({
         { src: "src/chrome-extension/public/32.png", dest: "./public" },
         { src: "src/chrome-extension/public/48.png", dest: "./public" },
         { src: "src/chrome-extension/public/192.png", dest: "./public" },
+        { src: "src/chrome-extension/contentCSS/", dest: "." },
       ],
     }),
   ],
+  resolve: {
+    alias: {
+      "@": resolve(__dirname, "./src"),
+    },
+  },
   server: {
     open: "/popup-local.html",
   },
@@ -29,7 +35,18 @@ export default defineConfig({
         popup: resolve(__dirname, "popup.html"),
         options: resolve(__dirname, "options.html"),
         background: resolve(__dirname, "src/background.ts"),
-        contentScripts: resolve(__dirname, "src/contentScripts/index.ts"),
+        praccTeamContentScript: resolve(
+          __dirname,
+          "src/contentScripts/praccTeam.ts"
+        ),
+        praccMainContentScript: resolve(
+          __dirname,
+          "src/contentScripts/praccMain.ts"
+        ),
+        praccSearchContentScript: resolve(
+          __dirname,
+          "src/contentScripts/praccSearch.ts"
+        ),
       },
       output: {
         entryFileNames: "[name].js",
