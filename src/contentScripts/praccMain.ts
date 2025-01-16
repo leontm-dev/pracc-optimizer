d1146b9e2db6166f80f404006bf61f7143686fe2
LeonTM <lanneken09@gmail.com>
2025-01-16 12:08:52 +0100

    Improved loading the extension icon

--- src/contentScripts/praccMain.ts
+++ src/contentScripts/praccMain.ts
@@ -1,14 +1,22 @@
-window.onload = async () => {
-  const userIcon = document.getElementsByClassName(
-    "css-1hy9t21"
-  )[1] as HTMLImageElement | null;
-  if (!userIcon) {
-    console.error("User icon not found");
-    return;
-  }
-  if (!userIcon.src) {
-    console.error("User icon src not found");
-    return;
-  }
-  chrome.storage.sync.set({ userIcon: userIcon.src });
-};
+const observer = new MutationObserver(function () {
+  (async () => {
+    console.log("praccMain content script loaded successfully");
+    const userIcon = document.getElementsByClassName(
+      "css-1hy9t21"
+    )[1] as HTMLImageElement | null;
+    if (!userIcon) {
+      console.error("User icon not found");
+      return;
+    }
+    if (!userIcon.src) {
+      console.error("User icon src not found");
+      return;
+    }
+    chrome.storage.sync.set({ userIcon: userIcon.src });
+    console.log("praccMain content script finished");
+  })();
+});
+
+const target = document.querySelector("body");
+const config = { childList: true };
+observer.observe(target!, config);
