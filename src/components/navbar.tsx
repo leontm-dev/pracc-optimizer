61ca8d9acb125a6c1034fe11f10352c5a2e6bdb8
LeonTM <lanneken09@gmail.com>
2025-01-16 13:02:07 +0100

    Added a donate button

--- src/components/navbar.tsx
+++ src/components/navbar.tsx
@@ -1,6 +1,6 @@
 // Imports
 
-import { CircleUser } from "lucide-react";
+import { CircleUser, Heart } from "lucide-react";
 
 // Components
 
@@ -8,6 +8,7 @@ import { ModeToggle } from "./mode-toggle";
 import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar";
 import { Separator } from "@/components/ui/separator";
 import { cn } from "@/lib/utils";
+import { Button } from "./ui/button";
 
 // Code
 
@@ -19,6 +20,11 @@ export function Navbar(props: Props) {
         <div className="flex flex-row items-center justify-between p-2">
             <h1 className={cn("text-foreground text-xl")}>Enhance your pracc experience</h1>
             <div className="flex flex-row gap-2">
+                <Button variant={"outline"} onClick={() => {
+                    window.open("https://buymeacoffee.com/leontm")
+                }}>
+                    <Heart className="fill-pink-500" />
+                </Button>
                 <ModeToggle />
                 <Avatar>
                     <AvatarFallback><CircleUser /></AvatarFallback>
